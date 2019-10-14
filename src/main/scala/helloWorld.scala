5e8b36de8020aa7259f6674b0b7fdd4d38aee29f
Shruti V. Joshi <shruti.joshi@infocepts.com>
2019-10-14 17:08:32 +0530

    migrate code

---
+++ src/main/scala/helloWorld.scala
@@ -0,0 +1,11 @@
+// Scala program to print Hello World!  
+//adding comment
+object Geeks  
+{ 
+    // Main Method  
+    def main(args: Array[String])  
+    { 
+        // prints Hello World 
+        println("Hello World!")  
+    } 
+} 
